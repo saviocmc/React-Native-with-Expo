4b8071db22fa6b2b019adf3d2213f01b38711830
Sávio Carlos Martins Costa <saviocmc@protonmail.com>
2021-09-11 18:19:30 -0300

    Aula 02 - 02 - Tela de Serviços

--- App.tsx
+++ App.tsx
@@ -1,21 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Services from "./src/telas/Services";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Alura!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <Services />
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/telas/Services/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { SafeAreaView, StatusBar, Text } from 'react-native';
+
+type Service = {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+}
+
+const services: Service[] = [
+    {
+        id: 1,
+        name: 'Banho',
+        price: 79.9,
+        description: 'Não dê banho no seu gato',
+    },
+    {
+        id: 2,
+        name: 'Vacina V4',
+        price: 89.9,
+        description: 'Uma dose da vacina V4. Seu gato precisa de duas',
+    },
+    {
+        id: 3,
+        name: 'Vacina Antirrábica',
+        price: 99.9,
+        description: 'Uma dose da vacina Antirrábica. Seu gato precisa de uma por ano',
+    },
+]
+
+export default function Services() {
+    return (
+        <SafeAreaView>
+            <StatusBar />
+            <Text>Serviços</Text>
+        </SafeAreaView>
+    )
+}
